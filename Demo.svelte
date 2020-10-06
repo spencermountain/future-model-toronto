6207837d12c215ceb072693894d6dca5ea51ae2c
spencer kelly <spencermountain@gmail.com>
2020-10-06 10:40:59 -0400

    boilerplate

---
+++ Demo.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<style>
+  .mt4 {
+    margin-top: 8rem;
+  }
+</style>
+
+<div class="main m4">
+  <div>
+    proof of concept interactions w\
+    <a href="https://www.stephenvelasco.com/" class="m4">stephen velasco</a>
+  </div>
+  <div class="mt4">hi</div>
+
+</div>
---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import json from '@rollup/plugin-json'
+
+function serve() {
+  let started = false
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true
+        require('child_process').spawn('serve', ['.'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true
+        })
+      }
+    }
+  }
+}
+
+export default {
+  input: `./app.js`,
+  output: {
+    sourcemap: false,
+    format: 'iife',
+    name: 'app',
+    file: 'build/bundle.js'
+  },
+  plugins: [
+    json(),
+    svelte({
+      dev: true,
+      css: true
+    }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
+    serve(),
+    livereload('.')
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
