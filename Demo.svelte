a048c4cae733221aa0d6c6eeb5caca615dd4d9e8
spencer kelly <spencermountain@gmail.com>
2020-10-06 11:35:00 -0400

    basic slider css

--- Demo.svelte
+++ Demo.svelte
@@ -1,18 +1,111 @@
 <script>
-
+  import data from './data'
 </script>
 
 <style>
-  .mt4 {
-    margin-top: 8rem;
+  .container {
+    position: relative;
+    min-height: 100vh;
+    max-height: 100vh;
+    height: 100%;
+  }
+  .col {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+    flex-wrap: nowrap;
+    align-self: stretch;
+  }
+
+  :global(body) {
+    background-color: #c7c8ca;
+    min-height: 100%;
+    margin: 0px;
+  }
+  #imgbox {
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    overflow: hidden;
+    height: 520px;
+    min-height: 520px;
+    border: 1px solid grey;
+  }
+  #slider {
+    position: absolute;
+    top: -180px;
+    border: 1px solid grey;
+    position: absolute;
+    left: -2500px;
+    z-index: 1;
+  }
+  .dir {
+    position: absolute;
+    font-size: 25px;
+    top: 10px;
+    z-index: 5;
+    color: #50617a;
+  }
+  #label {
+    position: absolute;
+    left: 30%;
+    width: 40%;
+    text-align: center;
+    font-size: 25px;
+    top: 10px;
+    z-index: 5;
+    color: #50617a;
+  }
+  .title {
+    font-size: 1rem;
+    color: #50617a;
+    text-align: left;
+    align-self: flex-start;
+    margin-left: 15px;
+    font-size: 0.9rem;
+    /* font-family: times new roman; */
+  }
+  #timeline {
+    flex: 1;
+    width: 100%;
+    border: 1px solid grey;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+  #arrow {
+    position: absolute;
+    left: 50%;
+    height: 40px;
+    width: 2px;
+    background-color: #50617a;
+    top: 60px;
+    z-index: 5;
   }
 </style>
 
-<div class="main m4">
-  <div>
-    proof of concept interactions w\
-    <a href="https://www.stephenvelasco.com/" class="m4">stephen velasco</a>
-  </div>
-  <div class="mt4">hi</div>
+<div class="container col">
 
+  <div class="title">
+    Toronto contstruction timeline - proof of concept for
+    <a href="https://www.stephenvelasco.com/">stephen velasco</a>
+  </div>
+
+  <div id="timeline">
+    {#each data as obj}
+      <div style="margin-top:40px;">{obj.name}</div>
+    {/each}
+  </div>
+
+  <div id="imgbox">
+    <div id="label">Toronto City Hall</div>
+    <div id="arrow" />
+    <div class="dir" style="left:20px;">⭠N</div>
+    <div class="dir" style="right:20px;">S⭢</div>
+    <div id="slider">
+      <img style="min-width:5000px;" src="./assets/from-west.jpeg" alt="" />
+    </div>
+  </div>
+  <!-- <img style="min-width:5000px;" src="./assets/from-west.jpeg" alt="" /> -->
 </div>
---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = [
+  // green
+  { name: 'The One', x: 934, height: 996, construction: true },
+  { name: 'YSL Reisdences', x: 1933, height: 299, construction: true },
+  { name: 'Sky Tower', x: 1927, height: 313, construction: true },
+  { name: 'Canada House', x: 3860, height: 230, construction: true },
+  { name: 'Rosedale on Bloor', x: 366, height: 186, construction: true },
+  // blue
+  { name: '625 Church', x: 851, height: 202, construction: false },
+  { name: '1 Scollard', x: 797, height: 155, construction: false },
+  { name: 'Cumberland Square', x: 1047, height: 238, construction: false },
+  { name: '1200 Bay', x: 918, height: 324, construction: false },
+  { name: 'Chelsea Green', x: 1970, height: 276, construction: false },
+  { name: '11 Bay', x: 3322, height: 270, construction: false },
+  { name: 'The Hub', x: 3377, height: 304, construction: false },
+  { name: 'Mirvish+Gehry', x: 3218, height: 329, construction: false },
+  { name: 'Union Park', x: 3663, height: 303, construction: false }
+]
